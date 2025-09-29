e39523204714e8df14a63c1b45961efcc2e158fc
Faizan Alam <faizansaikh786786f@gmail.com>
2025-09-29 23:33:32 +0530

    project

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://your-frontend-domain.com",
+    origin: "https://faizanalam123.netlify.app/",
     credentials: true
   }));
 app.use(express.json());
